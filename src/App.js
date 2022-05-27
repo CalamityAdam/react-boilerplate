0efa5c5d72b614d108740ed5377ac3403b809539
Adam Sisk <adamsisk91@gmail.com>
2022-05-26 22:47:42 -0500

    adds model solution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import React from 'react';
 import Container from 'react-bootstrap/container';
+import Carousel from 'react-bootstrap/carousel';
 
 function App() {
   return (
-    <Container>
-      <h1>hello, world</h1>
+    <Container className='mt-4'>
+      <h1>Multicats</h1>
+
+      {/* Add your code here 👇 */}
+
+      <Carousel>
+        {cats.map((cat) => (
+          <Carousel.Item key={cat.name}>
+            <img className='w-100' src={cat.imgUrl} alt={cat.name} />
+            <Carousel.Caption>
+              <h3>{cat.name}</h3>
+              <p>{cat.about}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+
+      {/* Add your code here ☝️ */}
     </Container>
   );
 }
 
 export { App };
+
+const cats = [
+  {
+    imgUrl: 'https://source.unsplash.com/FbhNdD1ow2g',
+    name: 'Dopey',
+    about: 'Just here for the snacks',
+  },
+  {
+    imgUrl: 'https://source.unsplash.com/IZaXUf6sLiA',
+    name: 'Grumpy',
+    about: "Tired of Dopey's snacking",
+  },
+  {
+    imgUrl: 'https://source.unsplash.com/LcAZcVWsCIo',
+    name: 'Sleepy',
+    about: 'ZzzzzzZZzzzz...',
+  },
+  {
+    imgUrl: 'https://source.unsplash.com/HrMD7MngiBE',
+    name: 'Sneezy',
+    about: 'Ah-choo!',
+  },
+];
