6b606ea1c81e2fbeb91282644414be8903695c40
Adam Sisk <adamsisk91@gmail.com>
2022-05-26 22:47:53 -0500

    adds tests

--- src/App.test.js
+++ src/App.test.js
@@ -8,3 +8,23 @@ it('should render', () => {
 
   expect(baseElement).toBeInTheDocument();
 });
+
+it('should render a carousel', () => {
+  const { baseElement } = render(<App />);
+
+  expect(baseElement.getElementsByClassName('carousel slide').length).toBe(1);
+});
+
+it('should render a carousel with 4 items', () => {
+  const { baseElement } = render(<App />);
+
+  expect(baseElement.getElementsByClassName('carousel-item').length).toBe(4);
+});
+
+it('each carousel item should have a caption', () => {
+  const { baseElement } = render(<App />);
+
+  [...baseElement.getElementsByClassName('carousel-item')].forEach((item) => {
+    expect(item.getElementsByClassName('carousel-caption').length).toBe(1);
+  });
+});
