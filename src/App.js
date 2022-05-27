5591fd54bd49e908ad9129c03a363c797d19df5e
Adam Sisk <adamsisk91@gmail.com>
2022-05-26 22:01:56 -0500

    adds bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Container from 'react-bootstrap/container';
 
 function App() {
   return (
-    <div>
+    <Container>
       <h1>hello, world</h1>
-    </div>
+    </Container>
   );
 }
 
