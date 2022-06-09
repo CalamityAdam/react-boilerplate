ccd2f5d3f5fff7ab7777656f822e797164c72a6b
Adam Sisk <adamsisk91@gmail.com>
2022-06-08 22:39:11 -0500

    fe-module-2.3 solution

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-function App() {
-  const [fact, setFact] = useState('');
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { fact: '' };
+  }
 
-  const fetchFact = () => {
+  fetchFact = () => {
     fetch(`https://uselessfacts.jsph.pl/random.json?language=en`)
       .then((res) => res.json())
-      .then((data) => setFact(data.text));
+      .then((data) => this.setState({ fact: data.text }));
   };
 
-  useEffect(() => {
-    fetchFact();
-  }, []);
+  componentDidMount() {
+    this.fetchFact();
+  }
 
-  return (
-    <header className='masthead d-flex align-items-center'>
-      <div className='container px-lg-5 text-center'>
-        <h1 className='mb-5 text-shadow'>{fact}</h1>
-        <button className='btn btn-primary btn-xl' onClick={fetchFact}>
-          Find Out More
-        </button>
-      </div>
-    </header>
-  );
+  render() {
+    return (
+      <header className='masthead d-flex align-items-center'>
+        <div className='container px-lg-5 text-center'>
+          <h1 className='mb-5 text-shadow'>{this.state.fact}</h1>
+
+          <button className='btn btn-primary btn-xl' onClick={this.fetchFact}>
+            Find Out More
+          </button>
+        </div>
+      </header>
+    );
+  }
 }
 
 export { App };
