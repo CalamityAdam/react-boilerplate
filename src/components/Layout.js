2a78c52982f2a8f692a9598afd34274cab8bd731
Adam Sisk <adamsisk91@gmail.com>
2022-06-14 22:27:32 -0500

    ads bonus

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
 
 function Layout() {
+  const navigate = useNavigate();
   return (
     <div>
-      <h1>📖 Bookkeeper 📊</h1>
+      <h1 onClick={() => navigate('/')}>📖 Bookkeeper 📊</h1>
 
       <nav>
         <Link to='invoices'>Invoices</Link> |{' '}
