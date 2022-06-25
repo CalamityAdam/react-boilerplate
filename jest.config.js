d58cd9d827a7216f4c719ce52623d64b82bc8e26
Adam Sisk <adamsisk91@gmail.com>
2022-06-24 21:36:58 -0500

    removes jest config & setup

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  verbose: true,
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
-};
-
-module.exports = config;
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 /** @jest-environment jsdom */
 import React from 'react';
+import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import { App } from './App';
 
